f619741bda8f9868f8108c5c830bd2c600a465e6
Chris-Haj <78118809+Chris-Haj@users.noreply.github.com>
2021-05-31 21:33:06 +0300

    Update TextAnalyzer.java

--- src/TextAnalyzer.java
+++ src/TextAnalyzer.java
@@ -1,21 +1,42 @@
-import java.io.IOException;
 import java.io.*;
+import java.io.IOException;
+import java.nio.Buffer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class TextAnalyzer extends Exception {
 
 private File file;
+private String path;
+private List<String> stopwords= new ArrayList<String>();
 
 
     public TextAnalyzer(String textPath) throws IOException {
-         if(textPath.substring(textPath.lastIndexOf('.'+1)).equals("txt"))
+         if(textPath.substring(textPath.length()-4,textPath.length()).equals(".txt")){
              file = new File(textPath);
+             path = textPath;
+         }
          else
              throw new IOException();
     }
     public void cleanText() throws CleanFailException{
+        String NormalPath = path.substring(0,path.lastIndexOf('.'));
+        File normal = new File(NormalPath+"Normalize.txt");
 
-        file = new File(file.getName()+"Normalize");
-        
+        try {
+            Scanner scan = new Scanner(new File("StopWords.txt"));
+            while(scan.hasNextLine()){
+            stopwords.add(scan.nextLine());
+            }
+
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
 
     }
 
